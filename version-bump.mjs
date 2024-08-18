a9284084199d617b5ecf8dbd3ef80d6d217619ba
Geert Eltink <geert.eltink@gmail.com>
2024-08-18 11:02:56 +0200

    build: include manifest file in version bumps

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,5 +7,3 @@ const targetVersion = process.env.npm_package_version;
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
-
-console.log(`Bumped version to ${targetVersion}`);
