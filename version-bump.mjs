a806bead872cad88c65e57e14c4824fcaccacd06
Geert Eltink <geert.eltink@gmail.com>
2025-08-03 12:27:31 +0200

    fix: oxlint warnings

--- version-bump.mjs
+++ version-bump.mjs
@@ -6,4 +6,4 @@ const targetVersion = process.env.npm_package_version;
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("./src/manifest.json", "utf8"));
 manifest.version = targetVersion;
-writeFileSync("./src/manifest.json", JSON.stringify(manifest, null, 2));
+writeFileSync("./src/manifest.json", JSON.stringify(manifest, undefined, 2));
